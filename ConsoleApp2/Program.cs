c2c74828d040761f0825bb5e2a6045bac72aa15d
User <User@DESKTOP-AMGP8UK>
2023-11-20 22:20:44 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Основы программирования в C#";
+        string[] words = input.Split(' ');
+
+        Array.Sort(words, (x, y) => y.Length.CompareTo(x.Length));
+
+        string result = string.Join(" ", words.Select(word => new string(word.Reverse().ToArray())));
+
+        Console.WriteLine(result);
+    }
+}
